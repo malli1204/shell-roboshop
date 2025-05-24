217c7e39e77206615e4a8e2d2509a788c44a18a1
malli <mallikarjuna2314@gmail.com>
2025-05-24 23:22:06 +0530

    special vars

--- payment.sh
+++ payment.sh
@@ -69,6 +69,9 @@ systemctl enable payment &>>$LOG_FILE
 systemctl start payment &>>$LOG_FILE
 VALIDATE $? "starting the system services"
 
+systemctl start payment &>>$LOG_FILE
+VALIDATE $? "restarting the system services"
+
 END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME - $START_TIME))
