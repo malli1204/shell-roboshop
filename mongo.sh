49b6d9ad803e89fffee24fb921e149b40209911a
malli <mallikarjuna2314@gmail.com>
2025-08-12 23:35:59 +0530

    roboshop

--- mongo.sh
+++ mongo.sh
@@ -41,7 +41,7 @@ systemctl enable mongod
 systemctl start mongod 
 VALIDATE $? "starting mongodb services.."
 
-sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "allowing all services.."
 
 systemctl restart mongod
