064284a717d924260fc5ddb9bdf26e5dbcd80349
malli <mallikarjuna2314@gmail.com>
2025-08-12 23:34:01 +0530

    roboshop

--- mongo.sh
+++ mongo.sh
@@ -22,7 +22,7 @@ else
     echo "you are running with root user" | tee -a $LOG_FILE
 fi
 
-validate(){
+VALIDATE(){
     if [ $1 -eq 0 ]
     then 
         echo -e "Installing $2 is $G success $N " | tee -a $LOG_FILE
